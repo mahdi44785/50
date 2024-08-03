e613bac8d8827323b461780fe93d4ec5cb37198c
mahdi44785 <mahdimug53@gmail.com>
2024-08-03 14:55:51 +0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
+namespace Magicscreen {
+    /**
+     * Addrees LEDs Linearly row first
+     */
+    //%blockid=screenmagicplotat
+    //%block="plot at index
+    //%index.min=0 index.max=25
+    export function plotAt (index: number): void{
+        index |=0
+        const y = Math.floor (index / 5);
+        const x = Math.floor (index % 5);
+        led.plot (x, y)
+
+    }
+
 	
-})
+}
